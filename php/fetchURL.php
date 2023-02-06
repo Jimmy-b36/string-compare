111a79a2f7a2194ffaca442e4efa5244503e419e
James Ball <jamie.j.ball@gmail.com>
2023-02-05 17:16:57 -0800

    add comments

--- php/fetchURL.php
+++ php/fetchURL.php
@@ -1,23 +1,25 @@
 <?php
 
-function getUrlContents(string $url)
-{
-  if ($url == '') {
-    return 'No URL selected or Error fetching URL data';
-  }
 
-  $site = file_get_contents("$url");
-  $site = preg_replace('/[^A-Za-z0-9<\/>\-]/', ' ', $site);
+// This function gets the contents of a URL and returns it as a string
+// The function checks if the <main> element exists, and if it does, it returns the contents of the <main> element
+// If the <main> element doesn't exist, it returns the contents of the <body> element
+// The function filters out all non-alphanumeric characters, including HTML tags
+
+function getUrlContents(string $url): string
+{
+  $page = file_get_contents("$url");
+  $page = preg_replace('/[^A-Za-z0-9<\/>\-]/', ' ', $page);
 
   // Check if <main> element exists
-  if (strpos($site, '<main') !== false) {
-    preg_match("/<main[^>]*>(.*?)<\/main>/is", $site, $matches);
-    return preg_replace('/\s+/', ' ', strip_tags($matches[1]));
-  } else {
-    preg_match("/<body[^>]*>(.*?)<\/body>/is", $site, $matches);
-    // If <main> element doesn't exist, return the entire page
+  if (strpos($page, '<main') !== false) {
+    preg_match("/<main[^>]*>(.*?)<\/main>/is", $page, $matches);
     return preg_replace('/\s+/', ' ', strip_tags($matches[1]));
   }
+
+  preg_match("/<body[^>]*>(.*?)<\/body>/is", $page, $matches);
+  // If <main> element doesn't exist, return the entire page
+  return preg_replace('/\s+/', ' ', strip_tags($matches[1]));
 }
 
 
