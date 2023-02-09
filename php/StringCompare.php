4daa2b66aff0135176ceddb8cd5aec663a80f4db
James Ball <jamie.j.ball@gmail.com>
2023-02-09 09:38:37 -0800

    add comments

--- php/StringCompare.php
+++ php/StringCompare.php
@@ -2,21 +2,34 @@
 
 class StringCompare
 {
+  /**
+   * Returns both old and new text highlighting the differences between them.
+   *
+   * @param string $old
+   * @param string $new
+   *
+   * @return array
+   */
   public function stringDiff(string $old, string $new): array
   {
     $placeholder = '_NEWLINE_';
 
+    // Check if text is too long
     if (str_word_count($old) > 10000 || str_word_count($new) > 10000) {
       return [
         'old' => 'Text is too long please submit 10000 words or less',
         'new' => 'Text is too long please submit 10000 words or less'
       ];
     }
+    // Replace all line breaks with a placeholder
     $modifiedOld = str_replace("\r\n", $placeholder, $old);
     $modifiedNew = str_replace("\r\n", $placeholder, $new);
+
+    // Remove all HTML tags
     $modifiedOld = strip_tags($modifiedOld);
     $modifiedNew = strip_tags($modifiedNew);
 
+    // Split the strings into arrays, separating them by spaces
     $diff = $this->diffArray(
       preg_split("/[\s]+/", $modifiedOld),
       preg_split("/[\s]+/", $modifiedNew)
@@ -24,6 +37,7 @@ class StringCompare
 
     $oldString = '';
     $newString = '';
+    // Loop through each array item and highlight the changes
     foreach ($diff as $item) {
       if (is_array($item)) {
         if ($item['deleted']) {
@@ -38,6 +52,7 @@ class StringCompare
       }
     }
 
+    // Replace the placeholder with line breaks
     $oldString = str_replace($placeholder, "<br/>", $oldString) . "<br/>";
     $newString = str_replace($placeholder, "<br/>", $newString) . "<br/>";
     return [
@@ -46,36 +61,52 @@ class StringCompare
     ];
   }
 
+  /**
+   * Calculates the difference between two arrays using the "longest common
+   * subsequence" algorithm.
+   *
+   * @param array $old
+   *   Array of strings from the old text.
+   * @param array $new
+   *   Array of strings from the new text.
+   *
+   * @return array
+   *   Array of strings containing the differences between the two arrays.
+   */
   public function diffArray(array $old, array $new): array
   {
-    # Create an array of all the lines in each file
+
+    // This is the matrix we will work with. It has the size of $old times $new.
     $matrix = array();
+
+    // We need to know the maximum length of the differences in the matrix.
     $maxlen = 0;
 
-    # Loop through each line of the old file
+    // Loop through the old array.
     foreach ($old as $oldIndex => $oldValue) {
-      # Find all the instances of this line in the new file
+      // Look for the same value in the new array.
       $newKeys = array_keys($new, $oldValue);
-      # Loop through each instance of this line in the new file
       foreach ($newKeys as $newIndex) {
-        # If this is the first instance of this line, set the value to 1
-        # Otherwise, increment the value by 1
+        // If we have a match, check the diagonal cell. If it exists, add 1 to the
+        // value. If it doesn't exist, it's 1.
         $matrix[$oldIndex][$newIndex] = isset($matrix[$oldIndex - 1][$newIndex - 1]) ? 
           $matrix[$oldIndex - 1][$newIndex - 1] + 1 : 1;
+
+        // If the length is greater than the maximum length, remember it.
         if ($matrix[$oldIndex][$newIndex] > $maxlen) {
-          # If this is the longest sequence of matching lines yet,
-          # store the length and the location of the sequence in the files
           $maxlen = $matrix[$oldIndex][$newIndex];
           $oldMax = $oldIndex + 1 - $maxlen;
           $newMax = $newIndex + 1 - $maxlen;
         }
       }
     }
+
+    // If there is no match, return an array with the old and new texts.
     if ($maxlen == 0) {
-      # If no lines match, return the entire files as a difference
       return array(array('deleted' => $old, 'inserted' => $new));
     }
-    # Otherwise, return the differences between the files
+
+    // If there is a match, return the differences.
     return array_merge(
       $this->diffArray(array_slice($old, 0, $oldMax), array_slice($new, 0, $newMax)),
       array_slice($new, $newMax, $maxlen),
