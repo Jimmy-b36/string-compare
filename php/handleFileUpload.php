9c96614b357881c9b764bb04851b443f5fac5aac
James Ball <jamie.j.ball@gmail.com>
2023-02-09 09:38:28 -0800

    add comments & try/catch

--- php/handleFileUpload.php
+++ php/handleFileUpload.php
@@ -1,16 +1,21 @@
 <?php
 include 'vendor/autoload.php';
+
+/**
+ * This function takes in a file extracts the text content and returns it as a string.
+ * @param mixed $file
+ * @return string
+ */
 function handleFileUpload($file)
 {
   $MIME_TYPES = ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/msword', 'text/plain', 'application/vnd.oasis.opendocument.text', 'application/msword', 'application/pdf'];
   $parser = new \Smalot\PdfParser\Parser();
   if (!isset($file['tmp_name'])) {
-    return;
+    return 'No file selected or Error uploading file';
   }
   if ($file['error'] !== 0) {
     return 'No file selected or Error uploading file';
   }
-
   if (!in_array($file['type'], $MIME_TYPES)) {
     return 'File type not supported';
   }
@@ -32,8 +37,12 @@ function handleFileUpload($file)
     return htmlspecialchars($text);
   }
   if ($file['type'] === 'text/plain') {
-    $data = file_get_contents($file['tmp_name']);
-    return htmlspecialchars($data);
+    try {
+      $text = file_get_contents($file['tmp_name']);
+      return htmlspecialchars($text);
+    } catch (Exception $e) {
+      return 'Error reading file';
+    }
   }
   if ($file['type'] === 'application/pdf') {
     $pdf = $parser->parseFile($file['tmp_name']);
@@ -41,32 +50,60 @@ function handleFileUpload($file)
     $text = preg_replace('/\t+/', '', $text);
     return htmlspecialchars($text);
   }
-  $data = file_get_contents($file['tmp_name']);
-  return htmlspecialchars($data);
+  try {
+    $text = file_get_contents($file['tmp_name']);
+    return htmlspecialchars($text);
+  } catch (Exception $e) {
+    return 'Error reading file';
+  }
+
 }
 
-
+/**
+ * This function takes in a docx file and returns the text content as a string.
+ * @param mixed $filename
+ * @return bool|string
+ */
 function read_file_docx($filename)
 {
+  // Create new ZIP archive
   $zip = new ZipArchive;
+  // Variable to store the contents of the document.xml file
   $striped_content = '';
+  // Variable to store the contents of the entire document
   $content = '';
+  // Check if the file exists
   if (!$filename || !file_exists($filename))
     return false;
+  // Open the document as a ZIP file
   if ($zip->open($filename) === TRUE) {
+    // Get the contents of the document.xml file
     if ($zip_entry = $zip->getStream("word/document.xml")) {
+      // Store the contents of the document.xml file in the $content variable
       $content = stream_get_contents($zip_entry);
+      // Close the file
       fclose($zip_entry);
     }
+    // Close the ZIP file
     $zip->close();
   } else {
     return false;
   }
+  // Replace any instances of </w:r></w:p></w:tc><w:tc> in the document with a space
   $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
+  // Replace any instances of </w:r></w:p> in the document with a new line
   $content = str_replace('</w:r></w:p>', "\r\n", $content);
+  // Strip out any HTML tags
   $striped_content = strip_tags($content);
+  // Return the stripped content
   return $striped_content;
 }
+
+/**
+ * This function takes in a odt file and returns the text content as a string.
+ * @param mixed $filename
+ * @return bool|string
+ */
 function read_file_odt($filename)
 {
   $dataFile = "content.xml";
@@ -90,6 +127,6 @@ function read_file_odt($filename)
     $zip->close();
   }
   // In case of failure return a message
-  return "File no`enter code here`t found";
+  return "Failed to open the file";
 }
 ?>
