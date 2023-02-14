09225c06ec46f93b24325a91e9f85f819fe69d38
James Ball <jamie.j.ball@gmail.com>
2023-02-14 08:32:51 -0800

    make more robust by fetching all divs

--- php/fetchURL.php
+++ php/fetchURL.php
@@ -1,48 +1,46 @@
 <?php
 
 
-// This function gets the contents of a URL and returns it as a string
-// The function checks if the <main> element exists, and if it does, it returns the contents of the <main> element
-// If the <main> element doesn't exist, it returns the contents of the <body> element
-// The function filters out all non-alphanumeric characters, including HTML tags
 
+/**
+ * Fetches the contents of a URL and returns the text content of the main element or the div elements
+ *
+ * @param string $url
+ * @return string
+ */
 function getUrlContents(string $url): string
 {
   $url = filter_var($url, FILTER_SANITIZE_URL);
   if (!preg_match('/^https?:\/\//', $url)) {
     return 'URL must start with http or https';
   }
-
   if (!filter_var($url, FILTER_VALIDATE_URL)) {
     return 'Invalid URL';
   }
   if (!preg_match('/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/', $url)) {
     return 'Invalid URL';
   }
-
   $url = strip_tags($url);
-
   try {
     $page = file_get_contents("$url");
   } catch (Exception $e) {
     return 'Error: ' . $e->getMessage();
   }
-
   $page = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', '', $page);
   $page = preg_replace('/[^A-Za-z0-9<\/>\-]/', ' ', $page);
-
   libxml_use_internal_errors(true);
   $dom = new DOMDocument();
   $dom->loadHTML($page);
   libxml_clear_errors();
-
   $mainElement = $dom->getElementsByTagName('main')->item(0);
   if ($mainElement) {
     return htmlentities($mainElement->textContent);
   }
-
-  $bodyElement = $dom->getElementsByTagName('body')->item(0);
-  return htmlentities($bodyElement->textContent);
+  $bodyElements = '';
+  $bodyElement = $dom->getElementsByTagName('div');
+  foreach ($bodyElement as $element) {
+    $bodyElements .= $element->textContent;
+  }
+  return trim(htmlentities($bodyElements));
 }
-
 ?>
