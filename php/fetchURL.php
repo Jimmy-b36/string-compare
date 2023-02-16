a8663f3a7ff2b49dcb6d2746758f2b463daf004e
James Ball <jamie.j.ball@gmail.com>
2023-02-16 13:53:55 -0800

    better HTML parsing

--- php/fetchURL.php
+++ php/fetchURL.php
@@ -3,7 +3,7 @@
 
 
 /**
- * Fetches the contents of a URL and returns the text content of the main element or the div elements
+ * Fetches the contents of a URL and returns the text content of the main-content element, the "main" element or the div elements
  *
  * @param string $url
  * @return string
@@ -26,25 +26,48 @@ function getUrlContents(string $url): string
   } catch (Exception $e) {
     return 'Error: ' . $e->getMessage();
   }
-  $page = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', '', $page);
   $page = preg_replace('/[^A-Za-z0-9<\/>\-]/', ' ', $page);
   libxml_use_internal_errors(true);
   $dom = new DOMDocument();
   $dom->loadHTML($page);
   libxml_clear_errors();
-  $mainElement = $dom->getElementsByTagName('main')->item(0);
+  // remove all <script> elements from the DOM
+  $scripts = $dom->getElementsByTagName('script');
+  while ($scripts && $scripts->length > 0)
+    $scripts->item(0)->parentNode->removeChild($scripts->item(0));
+  // remove all <style> elements from the DOM
+  $styles = $dom->getElementsByTagName('style');
+  while ($styles && $styles->length > 0)
+    $styles->item(0)->parentNode->removeChild($styles->item(0));
+
+  // if the "main-content" element exists, get the text content of all its descendants
+  $mainElement = $dom->getElementById('main-content');
   if ($mainElement) {
-    return htmlentities($mainElement->textContent);
+    $allElements = '';
+    $descendants = $mainElement->getElementsByTagName('*');
+    foreach ($descendants as $element) {
+      $allElements .= $element->textContent . "\r\n";
+    }
+    return trim(htmlentities($allElements));
+    // if the "main-content" element does not exist, get the text content of all the main elements
+  } else {
+    $mainTag = $dom->getElementsByTagName('main')->item(0);
+    if ($mainTag) {
+      $allElements = '';
+      foreach ($mainTag->childNodes as $child) {
+        $allElements .= $child->textContent . "\r\n";
+      }
+      return trim(htmlentities($allElements));
+      // if the "main" element does not exist, get the text content of all the div elements
+    } else {
+      $divs = $dom->getElementsByTagName('div');
+      $allElements = '';
+      foreach ($divs as $div) {
+        $allElements .= $div->textContent . "\r\n";
+      }
+      return trim(htmlentities($allElements));
+    }
   }
-  $allElements = '';
-  $headerElement = $dom->getElementsByTagName('header');
-  foreach ($headerElement as $element) {
-    $allElements .= $element->textContent;
-  }
-  $divElement = $dom->getElementsByTagName('div');
-  foreach ($divElement as $element) {
-    $allElements .= $element->textContent;
-  }
-  return trim(htmlentities($allElements));
+
 }
 ?>
